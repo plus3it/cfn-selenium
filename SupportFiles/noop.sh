7f60d38720510fbdb48f139c823e76e06fe515f2
Thomas H Jones II <github@xanthia.com>
2017-09-25 18:50:28 -0400

    Need this guy, for now

---
+++ SupportFiles/noop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# shellcheck disable=
+#
+# Dummy script to make Travis stop barking
+#
+# ...yeah, I know: fix up Travis's rules to avoid needing
+# this kind of kludge.
+#################################################################
+echo "${0}"
